824ba20e090b43b44e8e5dae443dd72fe3bfd24d
Michelle Lee <lee.michellejung@gmail.com>
2016-12-07 14:05:01 -0800

    don't remember what's different...

--- dom.js
+++ dom.js
@@ -1,1 +1,27 @@
  // JavaScript goes here
+
+// var count = document.querySelector('.count');
+// var updateCount = function(){
+//   count.textContent = //count of stickies
+// }
+
+
+var board = document.querySelector('.board')
+
+var count = document.querySelector('.count');
+var countStickies = function(event){
+  var counter = document.querySelectorAll('.sticky').length;
+  count.textContent(counter);
+}
+
+var button = document.querySelector('button');
+var handleButtonClick = function(event){
+  var sticky = document.createElement('div');
+  sticky.classList.add('sticky');
+  sticky.setAttribute("contenteditable",'true');
+  board.appendChild(sticky);
+  countStickies();
+}
+
+button.addEventListener('click', handleButtonClick)
+
